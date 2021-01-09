a5ab49494767af479731802bde5aea64611f20c5
Ruslan Sergeev <ruslansergeew@yandex.ru>
2021-01-09 04:15:39 +0300

    Добавлен Makefile для 2-й лабораторной. Добавлен пустой файл для 1-го задания

---
+++ lab-2/arrays_1.c
@@ -0,0 +1,17 @@
+/**
+ * Лабораторная 2. Задание 1.
+ *
+ * arrays_1.c -- вывод квадратной матрицы по заданному N
+ *
+ * Copyright (c) 2021, Ruslan Sergeev <ruslansergeew@yandex.ru>
+ *
+ * This code is licensed under a MIT-style license.
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+
+int main() 
+{
+    
+}
