9b43894aabaf9cd7147e8f1a260de1a56128dd8b
Ruslan Sergeev <ruslansergeew@yandex.ru>
2021-01-22 19:00:52 +0300

    Лабораторная 2. Задание 1.

--- lab-2/arrays_1.c
+++ lab-2/arrays_1.c
@@ -11,7 +11,14 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#define N 5
+
 int main() 
 {
-    
+    for (int i = 1; i <= N * N; i++) {
+        printf("%2u ", i);
+        if (i % N == 0) {
+            printf("\n");
+        }
+    } 
 }
