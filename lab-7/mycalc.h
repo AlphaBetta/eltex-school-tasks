038467f87eae7d368ae6587e226f31e328e9e53b
Ruslan Sergeev <ruslansergeew@yandex.ru>
2021-06-02 14:19:31 +0300

    #7.1| Написан заголовочный файл программы-калькулятора.

---
+++ lab-7/mycalc.h
@@ -0,0 +1,24 @@
+/**
+ * Лабораторная 7. Задание 1.
+ *
+ * mycalc.h -- Заголовочный файл проекта 
+ *
+ * Copyright (c) 2021, Ruslan Sergeev <ruslansergeew@yandex.ru>
+ *
+ * This code is licensed under a MIT-style license.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+
+/* Функция складывает два числа и возвращает результат */
+void add();
+
+/* Функция вычитает два числа и возвращает результат */
+void sub();
+
+/* Функция умножает два числа и возвращает результат */
+void mul();
+
+/* Функция делит два числа и возвращает результат */
+void div();
